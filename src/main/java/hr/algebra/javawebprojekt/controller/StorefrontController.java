6b7997c1c2cf2506f769316e44bb53a042d49e53
Ivan <someonenetko@hotmail.com>
2023-08-21 19:00:51 +0200

    Added categories page and controller method. Added category id as input param to products controller method - passing category name to the page model and filtering client-side. Updated all necessary pages. Also updated appearance using bootstrap.

--- src/main/java/hr/algebra/javawebprojekt/controller/StorefrontController.java
+++ src/main/java/hr/algebra/javawebprojekt/controller/StorefrontController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Map;
@@ -20,22 +21,39 @@ public class StorefrontController {
 
     private final StoreRepository storeRepository;
 
+    // http://localhost:8081/store
     @GetMapping("")
     public String home() {
         return "home";
     }
 
-    // http://localhost:8081/store/products
+
     @GetMapping("/products")
-    public String getAllProducts(Model model) {
+    public String getAllProducts(@RequestParam(required = false) Integer categoryID, Model model) {
         List<Proizvod> products = storeRepository.getAllProducts();
         List<Kategorija> categories = storeRepository.getAllCategories();
 
-        Map<Integer, String> categoryMap = categories.stream().collect(Collectors.toMap(Kategorija::getIdKategorija, Kategorija::getNaziv));
+        Map<Integer, String> categoryMap = categories.stream()
+                .collect(Collectors.toMap(Kategorija::getIdKategorija, Kategorija::getNaziv));
 
         model.addAttribute("products", products);
         model.addAttribute("categories", categories);
         model.addAttribute("categoryMap", categoryMap);
+
+        if (categoryID != null) {
+            String selectedCategoryName = categoryMap.get(categoryID);
+            if (selectedCategoryName != null) {
+                model.addAttribute("selectedCategoryName", selectedCategoryName);
+            }
+        }
         return "products";
+    }
+
+
+    @GetMapping("/categories")
+    public String getCategories(Model model) {
+        List<Kategorija> categories = storeRepository.getAllCategories();
+        model.addAttribute("categories", categories);
+        return "categories";
     }
 }
