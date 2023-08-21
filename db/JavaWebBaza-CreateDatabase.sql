72c491212c79fa001ff74b29a910051dcda506a7
Ivan <someonenetko@hotmail.com>
2023-08-21 23:04:00 +0200

    Added database files to project (er diagram, database creation, test data insertion).

---
+++ db/JavaWebBaza-CreateDatabase.sql
@@ -0,0 +1,92 @@
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+-- JavaWebBaza - Database Creation
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+-- DB
+
+use master
+go
+
+if db_id('JavaWebBaza') is not null
+begin
+	alter database JavaWebBaza
+	set single_user
+	with rollback immediate
+	drop database JavaWebBaza
+end
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+-- DB
+
+create database JavaWebBaza
+go
+
+alter database JavaWebBaza
+set single_user
+with rollback immediate
+
+use JavaWebBaza
+go
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+-- Entities
+
+create table [users]
+(
+	[username] nvarchar(300) primary key,
+	[password] nvarchar(300),
+	[enabled] bit
+)
+
+create table [authorities]
+(
+	[username] nvarchar(300) foreign key references [users]([username]),
+	[authority] nvarchar(300)
+)
+
+create table [Kategorija]
+(
+	[IDKategorija] int primary key identity,
+	[Naziv] nvarchar(300)
+)
+
+create table [Proizvod]
+(
+	[IDProizvod] int primary key identity,
+	[KategorijaID] int foreign key references [Kategorija]([IDKategorija]),
+	[Naziv] nvarchar(300),
+	[Cijena] float,
+	[DostupnaKolicina] int
+)
+
+create table [Racun]
+(
+	[IDRacun] int primary key identity,
+	[username] nvarchar(300) foreign key references [users]([username]),
+	[VrijemeKupovine] nvarchar(300),
+	[NacinKupovine] nvarchar(300),
+	[UkupnaCijena] float
+)
+
+create table [Stavka]
+(
+	[IDStavka] int primary key identity,
+	[RacunID] int foreign key references [Racun]([IDRacun]),
+	[ProizvodID] int foreign key references [Proizvod]([IDProizvod]),
+	[Kolicina] int
+)
+
+create table [Login]
+(
+	[IDLogin] int primary key identity,
+	[username] nvarchar(300) foreign key references [users]([username]),
+	[VrijemeLogina] nvarchar(300),
+	[IPAdresa] nvarchar(300)
+)
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+-- DB
+
+alter database JavaWebBaza
+set multi_user
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
---
+++ db/JavaWebBaza-InsertData.sql
@@ -0,0 +1,24 @@
+use JavaWebBaza
+go
+
+-- Categories
+insert into [Kategorija]([Naziv]) values
+('CPU'),
+('Motherboard'),
+('GPU');
+
+-- Products
+-- CPUs
+insert into [Proizvod]([KategorijaID], [Naziv], [Cijena], [DostupnaKolicina]) values
+(1, 'AMD Ryzen 9 5950X', 799.99, 100),
+(1, 'Intel Core i9-10900K', 499.99, 120);
+
+-- Motherboards
+insert into [Proizvod]([KategorijaID], [Naziv], [Cijena], [DostupnaKolicina]) values
+(2, 'MSI MPG B550 GAMING PLUS', 149.99, 75),
+(2, 'ASUS ROG Strix B450-F Gaming', 129.99, 80);
+
+-- GPUs
+insert into [Proizvod]([KategorijaID], [Naziv], [Cijena], [DostupnaKolicina]) values
+(3, 'NVIDIA GeForce RTX 3080', 699.99, 50),
+(3, 'AMD Radeon RX 6900 XT', 999.99, 40);
