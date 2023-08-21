bf4ea6b1a2e35f2226831b075ee12ba1bf5f92f1
Ivan <someonenetko@hotmail.com>
2023-08-21 13:34:41 +0200

    Initial commit

---
+++ src/main/java/hr/algebra/javawebprojekt/JavaWebProjektApplication.java
@@ -0,0 +1,13 @@
+package hr.algebra.javawebprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWebProjektApplication.class, args);
+    }
+
+}
