6d886bda338386bcd80f1d4ea08f2844e5a7d02a
Ivan <someonenetko@hotmail.com>
2023-08-22 21:19:57 +0200

    Minor changes to purchases.html for readability. Added datetime formatting util.

---
+++ src/main/java/hr/algebra/javawebprojekt/util/DateTimeUtils.java
@@ -0,0 +1,15 @@
+package hr.algebra.javawebprojekt.util;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateTimeUtils {
+
+    private DateTimeUtils() {
+    }
+
+    public static String formatLocalDateTime(LocalDateTime dateTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm:ss");
+        return dateTime.format(formatter);
+    }
+}
