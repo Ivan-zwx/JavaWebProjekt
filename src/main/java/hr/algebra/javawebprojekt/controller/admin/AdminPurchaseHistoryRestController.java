f8891488f98a8fdf297e95eab30330fa26f58484
Ivan <someonenetko@hotmail.com>
2023-08-27 01:24:39 +0200

    Added server-side filtering logic for AdminPurchaseHistoryRestController. Slightly updated the admin purchases view.

--- src/main/java/hr/algebra/javawebprojekt/controller/admin/AdminPurchaseHistoryRestController.java
+++ src/main/java/hr/algebra/javawebprojekt/controller/admin/AdminPurchaseHistoryRestController.java
@@ -5,9 +5,13 @@ import hr.algebra.javawebprojekt.repository.StoreRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/admin/purchases")
@@ -16,8 +20,51 @@ public class AdminPurchaseHistoryRestController {
 
     private final StoreRepository storeRepository;
 
+    /*
     @GetMapping("")
     public List<PurchaseHistoryDto> getCompletePurchaseHistory() {
         return storeRepository.getCompletePurchaseHistory();
+    }
+    */
+
+    @GetMapping("")
+    public List<PurchaseHistoryDto> getCompletePurchaseHistory(
+            @RequestParam(name = "username", required = false) String username,
+            @RequestParam(name = "startDate", required = false) String startDateStr,
+            @RequestParam(name = "endDate", required = false) String endDateStr) {
+
+        List<PurchaseHistoryDto> allPurchases = storeRepository.getCompletePurchaseHistory();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+
+        return allPurchases.stream()
+                .filter(dto -> dto.getRacunDetailsList().stream().anyMatch(racunDetails -> {
+                    if (username != null) {
+                        return username.equals(racunDetails.getRacun().getUsername());
+                    }
+                    return true;
+                }))
+                .map(dto -> new PurchaseHistoryDto(
+                        dto.getRacunDetailsList().stream()
+                                .filter(racunDetails -> {
+                                    if (startDateStr != null) {
+                                        LocalDateTime startDate = LocalDateTime.parse(startDateStr, formatter);
+                                        LocalDateTime invoiceDate = LocalDateTime.parse(racunDetails.getRacun().getVrijemeKupovine(), formatter);
+                                        if (invoiceDate.isBefore(startDate)) {
+                                            return false;
+                                        }
+                                    }
+                                    if (endDateStr != null) {
+                                        LocalDateTime endDate = LocalDateTime.parse(endDateStr, formatter);
+                                        LocalDateTime invoiceDate = LocalDateTime.parse(racunDetails.getRacun().getVrijemeKupovine(), formatter);
+                                        if (invoiceDate.isAfter(endDate)) {
+                                            return false;
+                                        }
+                                    }
+                                    return true;
+                                })
+                                .collect(Collectors.toList())
+                ))
+                .filter(dto -> !dto.getRacunDetailsList().isEmpty())
+                .collect(Collectors.toList());
     }
 }
